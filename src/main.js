1f2d1f42862b235b0a19d4777901a60398801f52
Istiak Kashem Jisan <istiakkashem35@gmail.com>
2025-11-05 02:26:47 +0600

    update and responsive

--- src/main.js
+++ src/main.js
@@ -132,7 +132,7 @@ function initEventCollection() {
   events.forEach((event, index) => {
     const card = document.createElement("div");
     card.className =
-      "group relative rounded-3xl overflow-hidden border-2 transition-all duration-300 h-[430px] flex items-end";
+      "group relative rounded-3xl overflow-hidden border-2 transition-all duration-300 h-[350px] sm:h-[430px] flex items-end";
     // card.style.borderColor = `#00B8A9`;
     card.style.backgroundImage = `url(${event.imageUrl})`;
     card.style.backgroundSize = "cover";
@@ -144,20 +144,22 @@ function initEventCollection() {
           <div class="absolute inset-0 w-full h-[105%] bg-linear-to-b from-neutral-900/10 to-neutral-950 overflow-hidden"></div> 
 
           <!-- Content -->
-          <div class="relative z-10 p-8 flex items-center justify-between w-full">
-            <div class="flex-1">
-              <h3 class="text-5xl font-bold text-white mb-2">
+          <div class="relative z-10 p-4 sm:p-8 flex max-sm:flex-col sm:items-center sm:justify-between w-full">
+            <div class="sm:flex-1 max-sm:mb-5">
+              <h3 class="text-3xl sm:text-5xl font-bold text-white mb-2">
                 ${event.title}
               </h3>
-              <p class="text-white/70 text-2xl">
+              <p class="text-white/70 text-2xl sm:text-2xl">
                 ${event.subtitle}
               </p>
             </div>
 
-            <!-- CTA Button -->
-            <button class="bg-primary hover:bg-primary-glow text-black rounded-full font-medium px-8 py-3 text-3xl shadow-xl transition-all duration-300 cursor-pointer">
-              Try Now
-            </button>
+            <div>
+              <!-- CTA Button -->
+              <button class="bg-primary hover:bg-primary-glow text-black rounded-full font-medium px-8 py-2 sm:py-3 text-xl sm:text-3xl shadow-xl transition-all duration-300 cursor-pointer">
+                Try Now
+              </button>
+            </div>
           </div>
         `;
 
